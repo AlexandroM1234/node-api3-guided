9950f76cac40649bf6ceecd6aeeb6417fc561ba6
Alex Martinez <alexmartinez0071234@gmail.com>
2020-04-08 13:56:54 -0400

    learned about middleware and how to make it

--- server.js
+++ server.js
@@ -1,15 +1,19 @@
-const express = require('express'); // importing a CommonJS module
+const express = require("express"); // importing a CommonJS module
+const morgan = require("morgan");
+const helmet = require("helmet");
 
-const hubsRouter = require('./hubs/hubs-router.js');
+const hubsRouter = require("./hubs/hubs-router.js");
 
 const server = express();
 
+server.use(helmet());
+server.use(morgan("dev"));
+// server.use(verify);
 server.use(express.json());
 
-server.use('/api/hubs', hubsRouter);
-
-server.get('/', (req, res) => {
-  const nameInsert = (req.name) ? ` ${req.name}` : '';
+server.use("/api/hubs", verify("mom"), hubsRouter);
+server.get("/", verify("wow"), (req, res) => {
+  const nameInsert = req.name ? ` ${req.name}` : "";
 
   res.send(`
     <h2>Lambda Hubs API</h2>
@@ -18,3 +22,20 @@ server.get('/', (req, res) => {
 });
 
 module.exports = server;
+
+// middleware
+// write and use a middleware that:
+/// -reads a "pass" key from req.query
+// if the pass === "mellon" , let the request continue
+// else respond with HTTP status code 400 and any message
+
+function verify(password) {
+  return function (req, res, next) {
+    const { pass } = req.query;
+    if (pass === password) {
+      next();
+    } else {
+      res.status(400).json({ message: "thats the wrong password fool" });
+    }
+  };
+}
